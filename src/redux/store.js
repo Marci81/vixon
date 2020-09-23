5f165ed1ab8f108b6a706b3689b97c73e7b49917
borymarton <bfmarton@gmail.com>
2020-09-23 15:48:10 +0200

    Remove redux logger temporarily will add redux dev tool later

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,9 @@
-import { createStore, applyMiddleware } from 'redux';
-import { persistStore } from 'redux-persist';
-import logger from 'redux-logger';
+import { createStore } from "redux";
+import { persistStore } from "redux-persist";
 
-import rootReducer from './root.reducer';
+import rootReducer from "./root.reducer";
 
-const middlewares = [logger];
-
-if (process.env.NODE_ENV === 'development') {
-  middlewares.push(logger);
-}
-
-export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer);
 
 export const persistor = persistStore(store);
 
